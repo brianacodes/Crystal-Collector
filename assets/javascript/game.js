7096ba586f0686082873030498e6429f90c43a23
unknown <exumcodes@gmail.com>
2019-05-16 19:33:33 -0600

    initial commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+alert("Heeeyyyy!");
+
+// FUNCTION DEFINITIONS
+//// End Game Function
+function endGame() {
+  alert("The game has ended");
+};
+
+endGame();
+
+$(document).ready(function () {
+
+  
+})
